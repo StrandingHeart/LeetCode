a849cdc8232bf08ff722f8a4f9052870d6ad41a5
zhangyong04 <zhangyong04@qianxin.com>
2021-02-22 23:17:01 +0800

    init linked list

---
+++ src/main/java/linkedlist/ListNode.java
@@ -0,0 +1,38 @@
+package linkedlist;
+
+/**
+ * @Author zhangyong
+ * @Date 2021/2/22 23:15
+ */
+public class ListNode {
+    private int val;
+    private ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+}
